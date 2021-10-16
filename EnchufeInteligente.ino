04dee5d2d4f8538c4a7d9189c433ab930980322f
Saul Nuñez <saul.nunez99@gmail.com>
2021-10-16 12:49:45 -0600

    Guardar informacion de conexion wifi y logica para volver a cargar esos datos

--- EnchufeInteligente.ino
+++ EnchufeInteligente.ino
@@ -22,6 +22,23 @@ void wifiDeviceSetupSetup(){
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
     request->send(LittleFS, "/wifi_info.html", String(), false);
   });
+  server.on("/wifi_info", HTTP_POST, [](AsyncWebServerRequest *request){
+    String ssid;
+    String password;
+
+    AsyncWebParameter* ssidParam = request->getParam("wifi");
+    ssid = ssidParam->value().c_str();
+    AsyncWebParameter* passwordParam = request->getParam("password");
+    password = passwordParam->value().c_str();
+
+    File f = LittleFS.open("/wifi.conf", "w");
+    if(!f){
+      return;
+    }
+    f.write(("ssid:" + ssid + "\n" + "password:" + password).c_str());
+    f.close();
+    ESP.restart();
+  });
   server.begin();
   Serial.println("HTTP server started");
 }
@@ -29,6 +46,20 @@ void wifiDeviceSetupSetup(){
 void normalOperationSetup(){
   //Encender wifi
   WiFi.mode(WIFI_STA);
+
+  File file = LittleFS.open("/wifi.conf", "r");
+  String wifiFileContents = file.readString();
+  file.close();
+
+  int newLineSeparator = wifiFileContents.indexOf('\n');
+  String ssidFull = wifiFileContents.substring(0, newLineSeparator);
+  String passwordFull = wifiFileContents.substring(newLineSeparator);
+
+  String ssid = ssidFull.substring(5);
+  String password = passwordFull.substring(9);
+
+  WiFi.begin(ssid, password);
+
   fauxmo.addDevice("Enchufe Uno");
 
   fauxmo.setPort(80);
@@ -47,6 +78,12 @@ void setup() {
 if(!LittleFS.begin()){
     Serial.println("An Error has occurred while mounting LittleFS");
     return;
+  }
+
+  if(!LittleFS.exists("/wifi.conf")){
+    normalOperationSetup();
+  } else {
+    wifiDeviceSetupSetup();
   }
 }
 
